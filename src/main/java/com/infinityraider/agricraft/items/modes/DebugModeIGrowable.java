935ef55cc8f6600872e2c1ef8655705950a43e46
CodesCubesAndCrashes <CodesCubesAndCrashes@users.noreply.github.com>
2017-09-16 15:03:17 -0400

    Stash
    Test and squash.

--- src/main/java/com/infinityraider/agricraft/items/modes/DebugModeIGrowable.java
+++ src/main/java/com/infinityraider/agricraft/items/modes/DebugModeIGrowable.java
@@ -13,6 +13,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.event.HoverEvent;
 import net.minecraft.world.World;
 
 /**
@@ -23,8 +24,17 @@ public class DebugModeIGrowable extends DebugMode {
 
     @Override
     public String debugName() {
-        return "igrowable interface test";
+        return "test igrowable interface";
     }
+
+    // Saved copies of the texts that do not change.
+    private static final String chatTrue  = "\u00A72True  \u00A7r";
+    private static final String chatFalse = "\u00A74False \u00A7r";
+    private static final String chatNotIG = "\u00A78----  ----  Block is not IGrowable.\u00A7r";
+    private static final String chatInfo  =
+            "x, y, z | canGrow | canUseBonemeal | BlockName\n" +
+            "If this is a valid target, then all three methods\n" +
+            "will get called (regardless of individual results).";
 
     @Override
     public void debugActionBlockClicked(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
@@ -32,42 +42,34 @@ public class DebugModeIGrowable extends DebugMode {
             return;
         }
 
-        player.addChatComponentMessage(new TextComponentString("Searching for an IGrowable TileEntity at: " + pos));
+        // Start the string with the position of the clicked on block.
+        StringBuilder outputRaw = new StringBuilder(String.format("\u00A77%1$4d,%2$4d,%3$4d\u00A7r ", pos.getX(), pos.getY(), pos.getZ()));
+
+        // Check if the clicked on block has the IGrowable interface.
         IGrowable crop = WorldHelper.getBlock(world, pos, IGrowable.class).orElse(null);
-
         if (crop == null) {
-            player.addChatComponentMessage(new TextComponentString("None found there."));
-            return;
+            // If it does not, add a nicely formatted report, and skip the rest.
+            outputRaw.append(chatNotIG);
         } else {
-            player.addChatComponentMessage(new TextComponentString("Found: " + crop));
+            // Otherwise run the tests and record the results.
+            IBlockState state = world.getBlockState(pos);
+            outputRaw.append(crop.canGrow(world, pos, state, false)     ? chatTrue : chatFalse);
+            outputRaw.append(crop.canUseBonemeal(world, world.rand, pos, state) ? chatTrue : chatFalse);
+            outputRaw.append("\u00A73");
+            outputRaw.append(crop.toString().substring(5));
+            outputRaw.append("\u00A7r");
+            crop.grow(world, world.rand, pos, state);
         }
 
-        IBlockState state = world.getBlockState(pos);
+        // Turn the String into a chat message. Add explanatory information in a hover box.
+        outputRaw.append(" \u00A78[...]\u00A7r");
+        TextComponentString outputComponent = new TextComponentString(outputRaw.toString());
+        TextComponentString hoverComponent  = new TextComponentString(chatInfo);
+        HoverEvent hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverComponent);
+        outputComponent.getStyle().setHoverEvent(hoverEvent);
 
-        player.addChatComponentMessage(new TextComponentString("Calling canGrow."));
-        boolean cangrow = crop.canGrow(world, pos, state, false);
-        player.addChatComponentMessage(new TextComponentString("Result: " + cangrow));
-
-        if (!cangrow) {
-            player.addChatComponentMessage(new TextComponentString("Stopping. Should NOT consume bonemeal."));
-            return;
-        }
-
-        player.addChatComponentMessage(new TextComponentString("Calling canUseBonemeal."));
-        boolean canuseb = crop.canUseBonemeal(world, world.rand, pos, state);
-        player.addChatComponentMessage(new TextComponentString("Result: " + canuseb));
-
-        if (!canuseb) {
-            player.addChatComponentMessage(new TextComponentString("Stopping. Should consume bonemeal anyhow."));
-            return;
-        }
-
-        player.addChatComponentMessage(new TextComponentString("Calling grow."));
-        crop.grow(world, world.rand, pos, state);
-
-        world.playEvent(2000, pos, 4);
-        player.addChatComponentMessage(new TextComponentString("Finished."));
-
+        // Now send the completed message.
+        player.addChatComponentMessage(outputComponent);
     }
 
     @Override
